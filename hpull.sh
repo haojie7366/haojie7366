41e4ab7ee50547e17ceef3501c6dc4dd4cd00856
haojie7366 <haojie7366@gmail.com>
2023-07-11 08:40:23 +0800

    2023年 07月 11日 星期二 08:40:23 CST

--- hpull.sh
+++ hpull.sh
@@ -1,10 +1,8 @@
-cd /home/me/blog
 source path.sh
 npm i
-hexo g
-cd /home/me 
-cp -r blog/public/* haojie7366.github.io/
-cd haojie7366.github.io
+hexo g 
+cp -r public/* ../haojie7366.github.io/
+cd ../haojie7366.github.io
 git add --all
 git commit -m "$(date)"
 git push
